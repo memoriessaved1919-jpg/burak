843a2cdd346adef3c6918712ac58fb5928efe835
Muhammadumar <memoriessaved1919@gmail.com>
2026-09-23 12:41:12 +0900

    feat: connect via mongoose

--- src/server.ts
+++ src/server.ts
@@ -12,3 +12,14 @@ dotenv.config();               // .env fayldagi barcha qiymatlarni process.env i
 
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
+import mongoose from 'mongoose'
+
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log("MongoDB connection succeed");
+        const PORT = process.env.PORT ?? 3003
+    })
+    .catch((err) => console.log("ERROR on connection MongoDB", err))
+
