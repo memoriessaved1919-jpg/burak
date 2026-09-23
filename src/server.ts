064f6fda46ae7aa17481af96bff739a9e4186e54
Muhammadumar <memoriessaved1919@gmail.com>
2026-09-23 11:45:36 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -9,3 +9,6 @@
 //import moment from 'moment';     // const moment = require('moment')
 import dotenv from 'dotenv'    // dotenv paketini import qilyapmiz — u .env faylni o'qish uchun kerak
 dotenv.config();               // .env fayldagi barcha qiymatlarni process.env ichiga yozib qo'yadi       
+
+
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
